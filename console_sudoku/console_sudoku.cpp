32df0c672fd36816c2e40a3de35a83bb6264d5de
ubormaci <ubormaci@gmail.com>
2024-08-03 14:33:54 +0300

    Game is playable and confirmed to be working

--- console_sudoku/console_sudoku.cpp
+++ console_sudoku/console_sudoku.cpp
@@ -7,8 +7,11 @@
 using namespace std;
 
 int table[9][9];
+bool locked[9][9]; // locked values
 int n = 9;
 // table[y][x] in terms of coordinates, or table[i][j] in terms of for loop structure
+
+bool nums[10];
 
 void initializeTable();
 void displayTable();
@@ -16,6 +19,10 @@ void commandWait();
 void startGame();
 void saveGame();
 void loadGame();
+void stateCheck();
+bool checkCollection();
+void resetCollection();
+void endGame();
 
 int main()
 {
@@ -59,6 +66,10 @@ void initializeTable()
         {
             string curchar = line.substr(iter++, 1);
             table[i][j] = stoi(curchar);
+            if (table[i][j] != 0)
+            {
+                locked[i][j] = true;
+            }
         }
     }
 
@@ -69,7 +80,7 @@ void initializeTable()
 
 void displayTable()
 {
-    system("cls");
+    //system("cls");
 
     cout << "\n\n";
     cout << "       ";
@@ -115,7 +126,7 @@ void commandWait()
 
     if (get == "exit")
     {
-        return;
+        exit(0);
     }
 
     if (get == "new")
@@ -133,6 +144,7 @@ void commandWait()
     if (get == "load")
     {
         loadGame();
+        displayTable();
     }
 
     if (get == "help")
@@ -146,17 +158,150 @@ void commandWait()
     int y = stoi(get.substr(2, 1));
     int value = stoi(get.substr(4, 1));
 
-    table[y][x] = value;
+    if (locked[y][x] == false)
+    {
+        table[y][x] = value;
+    }
+    else
+    {
+        cout << "Sorry, that can't be changed.";
+        commandWait();
+    }
+
+    stateCheck();
 
     displayTable();
 }
 
 void saveGame()
 {
-    // TODO
+    ofstream out("savegame.txt");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            out << table[i][j];
+        }
+    }
 }
 
 void loadGame()
 {
-    // TODO
+    ifstream in("savegame.txt");
+
+    string line;
+    getline(in, line);
+
+    int iter = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            string curchar = line.substr(iter++, 1);
+            table[i][j] = stoi(curchar);
+        }
+    }
+}
+
+void stateCheck()
+{
+    // first off, we check if we are even *ready* checking
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (table[i][j] == 0)
+            {
+                return;
+            }
+        }
+    }
+
+    //cout << "\ngrid is filled in";
+
+    bool done = true;
+
+    // checking by lines:
+    for (int i = 0; i < n; i++)
+    {
+        resetCollection();
+        for (int j = 0; j < n; j++)
+        {
+            nums[table[i][j]] = true;
+        }
+        if (!checkCollection())
+        {
+            done = false;
+            return;
+        }
+    }
+
+    //cout << "\nrows are alright";
+
+    // checking by columns
+    for (int i = 0; i < n; i++)
+    {
+        resetCollection();
+        for (int j = 0; j < n; j++)
+        {
+            nums[table[j][i]] = true;
+        }
+        if (!checkCollection())
+        {
+            done = false;
+            return;
+        }
+    }
+
+    //cout << "\ncolumns are alright";
+
+    //checking by cubes
+    for (int i = 0; i < n; i+=3)
+    {
+        for (int j = 0; j < n; j+=3)
+        {
+            resetCollection();
+
+            for (int k = 0; k < 3; k++)
+            {
+                for (int l = 0; l < 3; l++)
+                {
+                    nums[table[i + k][j + l]] = true;
+                }
+            }
+
+            if (!checkCollection())
+            {
+                done = false;
+                return;
+            }
+        }
+    }
+
+    // if we've reached this point, it means that our sudoku is finished
+    cout << "\n\nCongratulations, you successfully completed this sudokue! :D";
+    cout << "\nWrite 'new' if you'd like to play another, or 'exit' if you were satisfied.";
+    cout << "\nThank you for playing!";
+    commandWait();
+}
+
+bool checkCollection()
+{
+    for (int i = 1; i < n + 1; i++)
+    {
+        if (nums[i] == false)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+void resetCollection()
+{
+    for (int i = 1; i < n + 1; i++)
+    {
+        nums[i] = false;
+    }
 }
