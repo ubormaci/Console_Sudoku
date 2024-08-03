e1d10a3213e85904110ad50ae4d77d84d64ea90b
ubormaci <ubormaci@gmail.com>
2024-08-03 12:24:06 +0300

    added starting configurations

--- console_sudoku/console_sudoku.cpp
+++ console_sudoku/console_sudoku.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <map>
+#include <string>
+#include <fstream>
 using namespace std;
 
 int table[9][9];
 
+// commands:
+// "new" - start a new game
+// "save" - save the current state of the board
+// "load" - load the last saved state
+// "help" - print out all other commands
+// "{x} {y} {value}" - place a value at x,y coordinates
+
+void initializeTable();
+void displayTable();
+
 int main()
 {
         
+}
+
+void initializeTable()
+{
+    const int max_lines = 36604;
+    ifstream inread("starting_configurations.txt");
+
+}
+
+void displayTable()
+{
+
 }
